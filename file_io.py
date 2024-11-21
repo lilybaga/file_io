d66ea545ba9f6b39b536186e096be3bd43469147
lilybaga <lilybagar@gmail.com>
2024-11-21 16:38:43 +0300

    Добавлена обработка исключений

--- file_io.py
+++ file_io.py
@@ -2,16 +2,29 @@ from tkinter import *
 import requests
 from tkinter import filedialog as fd
 from tkinter import ttk
+from tkinter import messagebox as mb
+
 
 
 def upload():
-    filepath = fd.askopenfilename()
-    if filepath:
-        files = {'file': open(filepath, 'rb')}
-        response = requests.post('https://file.io', files=files)
-        if response.status_code == 200:
-            download_link = response.json()['link']
-            entry.insert(0, download_link)
+    try:
+        filepath = fd.askopenfilename()
+        if filepath:
+            with open(filepath, 'rb') as f:
+                files = {'file': f}
+                response = requests.post('https://file.io', files=files)
+                response.raise_for_status()  # Проверка на ошибки HTTP
+                download_link = response.json().get('link')
+                if download_link:
+                    entry.delete(0, END)
+                    entry.insert(0, download_link)
+                else:
+                    raise ValueError("Не удалось получить ссылку для скачивания")
+    except ValueError as ve:
+        mb.showerror("Ошибка", f"Произошла ошибка: {ve}")
+    except Exception as e:
+        mb.showerror("Ошибка", f"Произошла ошибка: {e}")
+
 
 window = Tk()
 window.title("Сохранение файлов в облаке")
